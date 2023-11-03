7395fb04e9ff534b40ad4a60611cd5980a948124
Nithya2303 <122733926+Nithya2303@users.noreply.github.com>
2023-11-03 14:00:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,5 +11,5 @@ app.use(cors());
 
 app.use("/api/tasks", tasks);
 
-const port = process.env.PORT || 5000;
+const port =  5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
